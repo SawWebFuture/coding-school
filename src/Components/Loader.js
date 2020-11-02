70ff94b509ff2199da9e9b46d6389a7253fd6e9c
Scott Alan Williams <scottalanw@gmail.com>
2020-11-02 11:55:46 -0500

    Add loading item

---
+++ src/Components/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function loader() {
+    return (
+        <div className="flex justify-center">
+            <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        </div>
+    )
+}
+
+export default loader
--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,38 +1,53 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom'
+import Loader from '../Components/Loader'
 
 function Product() {
     const { id } = useParams()
     const url = `https://5fa0297fe21bab0016dfce9e.mockapi.io/coding-school/${id}`
-    const [product, setProduct] = useState(null);
+    const [product, setProduct] = useState({
+        loading: false,
+        data: null
+    })
 
-    let content = null;
+    let content = null
 
     useEffect(() => {
+        setProduct({
+            loading: true,
+            data: null
+        })
         axios.get(url)
             .then(response => {
-                setProduct(response.data)
+                setProduct({
+                    loading: false,
+                    data: response.data
+                })
             })
     }, [url])
 
-    if(product){
+    if(product.loading){
+        content = <Loader></Loader>
+    }
+    
+    if(product.data){
         content =
             <div>
                 <h1 className="text-2xl font-bold mb-3">
-                    {product.name}
+                    {product.data.name}
                 </h1>
                 <div>
                     <img
-                        src={product.avatar}
-                        alt={product.name}
+                        src={product.data.avatar}
+                        alt={product.data.name}
                     />
                 </div>
                 <div className="font-bold text-xl mb-3">
-                    $ {product.price}
+                    $ {product.data.price}
                 </div>
                 <div>
-                    {product.description}
+                    {product.data.description}
                 </div>
             </div>
     }
