8a93c1393b8426498244c0bbdc4947f65bde0ce0
Scott Alan Williams <scottalanw@gmail.com>
2020-11-02 15:11:23 -0500

    Finish up

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,12 +1,43 @@
-import React from 'react'
-import OpenEyes from '../Components/OpenEyes'
+import React, { useState, useEffect } from 'react'
+import Loader from '../Components/Loader'
+import ProductCard from '../Components/ProductCard'
+import { useAxiosGet } from '../Hooks/HttpRequests'
 
 function Home() {
+    const url = `https://5fa0297fe21bab0016dfce9e.mockapi.io/products?page=1&limit=10`
+
+    let products = useAxiosGet(url)
+
+    let content = null
+
+    if(products.error){
+        content = <p>
+            There was an Error. Please try again later.
+        </p>
+    }
+
+    if(products.loading){
+        content = <Loader></Loader>
+    }
+
+    if(products.data){
+        content =
+        products.data.map((product, key) => 
+            <div key={product.id}>
+                <ProductCard 
+                    product={product}
+                />
+            </div>
+        )
+    }
+
     return (
         <div>
-            <h1 className="font-bold text-2x1">This is the home page</h1>
+            <h1 className="font-bold text-2x1 mb-3">
+                Coding School
+            </h1>
 
-            <OpenEyes name="Scottchi"/>
+            {content}
 
         </div>
     )
--- src/Views/Product.js
+++ src/Views/Product.js
@@ -2,40 +2,15 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
 import Loader from '../Components/Loader'
+import { useAxiosGet } from '../Hooks/HttpRequests'
 
 function Product() {
     const { id } = useParams()
-    const url = `https://5fa0297fe21bab0016dfce9e.mockapi.io/coding-school/${id}`
-    const [product, setProduct] = useState({
-        loading: false,
-        data: null,
-        error: false
-    })
+    const url = `https://5fa0297fe21bab0016dfce9e.mockapi.io/products/${id}`
+    
+    let product = useAxiosGet(url)
 
     let content = null
-
-    useEffect(() => {
-        setProduct({
-            loading: true,
-            data: null,
-            error: false
-        })
-        axios.get(url)
-            .then(response => {
-                setProduct({
-                    loading: false,
-                    data: response.data,
-                    error: false
-                })
-            })
-            .catch(() => {
-                setProduct({
-                    loading: false,
-                    data: null,
-                    error: true
-                })
-            })
-    }, [url])
 
     if(product.error){
         content = <p>
